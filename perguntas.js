7874833989de035a0ba3c2133a11ba6356b8c902
splichal07 <159157499+splichal07@users.noreply.github.com>
2024-10-22 11:38:44 -0300

    22/10/2024

--- perguntas.js
+++ perguntas.js
@@ -1,32 +1,41 @@
-criaCartao(
-    'Geografia',
-    'Qual a capital do Paraná?',
-    'A capital do Paraná é Curitiba'
-)
-criaCartao(
-    'Geografia',
-    'Qual a capital de Minas Gerais?',
-    'A Capital de Minas Gerais é Belo Horizonte'
-)
-criaCartao(
-    'Geografia',
-    'Qual a capital de São Paulo?',
-    'A Capital de São PAulo é São Paulo'
-)
-criaCartao(
-    'Geografia',
-    'Qual a capital de Tocantins?',
-    'A Capital de Tocantins é Palmas'
-)
-criaCartao(
-    'Geografia',
-    'Qual a capital do Rio Grande do Sul?',
-    'A Capital do Rio Grande do Sul é Porto Alegre'
-)
-criaCartao(
-    'Geografia',
-    'Qual a capital da Bahia?',
-    'A Capital da Bahia é Salvador'
+criaCartao (
+    'Suco',
+    'O suco de vocês é natural?',
+    'Sim.'
+
 )
 
+criaCartao (
+    'Suco',
+    'Vocês fazem entrega?',
+    'Sim.'
 
+)
+
+criaCartao (
+    'Suco',
+    'Qual o melhor suco de Londrina?',
+    'O do Point do Suco.'
+
+)
+
+criaCartao (
+    'Suco',
+    'O quais sabores vocẽs tem?',
+    'Temos laranja, maracujá, uva, goiaba, laranja com morango.'
+
+)
+
+criaCartao (
+    'Suco',
+    'Quanto custa o litro?',
+    '1l 13,00 - 2l 24,00 - 3l 33,00 - 5l 54,00.'
+
+)
+
+criaCartao (
+    'Suco',
+    'Vocês vendem outras coisas?',
+    'Sim.'
+
+)
